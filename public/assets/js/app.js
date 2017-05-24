31a18655171f712e3d43b49d23964d380de95388
CaroTakata <charlotte.avelar@gmail.com>
2017-05-24 18:29:15 -0500

    rutas arregladas

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,13 +1,24 @@
-var app = angular.module("myApp", ["ngRoute"]);
-        app.config(function ($routeProvider) {
+var app = angular.module("myApp", ["ngRoute"])
+        .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+            $locationProvider.hashPrefix('');
             $routeProvider                
                 .when("/", {
                     templateUrl: "templates/post.html"
-                });
-        });
-        app.run(['$rootScope', function ($rootScope) {
-            $rootScope.$on('$routeChangeSuccess', function (next, current) {
-                console.log("TESTING");
+                })
+                .when("/publicar-receta", {
+                    templateUrl: "templates/publicarreceta.html"
+                })
+                .when("/mi-perfil", {
+                    templateUrl: "templates/miperfil.html"
+                })
+                .when("/favoritos", {
+                    templateUrl: "templates/favoritos.html"
+                })
+                .when("/editar-perfil", {
+                    templateUrl: "templates/editarperfil.html"
+                })
+            .otherwise({
+                redirectTo: '/' 
             });
         }]);
         
