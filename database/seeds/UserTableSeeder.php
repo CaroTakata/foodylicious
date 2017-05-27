7aa77c501a732f4b3d6ed888a88453b2ca3a3c2e
CaroTakata <charlotte.avelar@gmail.com>
2017-05-26 19:10:08 -0500

    birthdates

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -18,10 +18,10 @@ class UserTableSeeder extends Seeder
         DB::table('users')->delete();
 
         $users = array(
-                ['name' => 'Fernando Cantú', 'email' => 'fernandoc@gmail.com', 'password' => Hash::make('secret'), 'gender' => '1', 'avatar' => 'apple.jpg'],
-                ['name' => 'Christina Sevilleja', 'email' => 'christinas@gmail.com', 'password' => Hash::make('secret'), 'gender' => '2', 'avatar' => 'chefsito.jpg'],
-                ['name' => 'Omar Ríos', 'email' => 'omarr@gmail.com', 'password' => Hash::make('secret'), 'gender' => '1', 'avatar' => 'garfield.jpg'],
-                ['name' => 'Iván López', 'email' => 'ivanl@hotmail.com', 'password' => Hash::make('secret'), 'gender' => '1', 'avatar' => 'gordon.jpg'],
+                ['name' => 'Fernando Cantú', 'email' => 'fernandoc@gmail.com', 'password' => Hash::make('secret'), 'gender' => '1', 'birthdate' => '1994-03-14', 'avatar' => 'chefsito.jpg'],
+                ['name' => 'Christina Sevilleja', 'email' => 'christinas@gmail.com', 'password' => Hash::make('secret'), 'gender' => '2', 'birthdate' => '1990-05-30', 'avatar' => 'apple.jpg'],
+                ['name' => 'Omar Ríos', 'email' => 'omarr@gmail.com', 'password' => Hash::make('secret'), 'gender' => '1', 'birthdate' => '1996-11-17', 'avatar' => 'garfield.jpg'],
+                ['name' => 'Iván López', 'email' => 'ivanl@hotmail.com', 'password' => Hash::make('secret'), 'gender' => '1', 'birthdate' => '1997-01-20','avatar' => 'gordon.jpg'],
         );
             
         // Loop through each user above and create the record for them in the database
